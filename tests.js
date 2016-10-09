67aa135c4edd8b03bfa0e6ba854b3787a7557470
alexb5dh <alexb5dh@gmail.com>
2016-10-09 10:00:05 +0300

    tests update

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 function Assert(data, disagreementFactor) {
     this.allPairsHaveDisagreement = function (pairs) {
         for (let pair of pairs) {
-            console.assert(pair.difference >= disagreementFactor, pair, disagreementFactor);
+            console.assert(pair.difference >= disagreementFactor, { pair: pair, disagreementFactor: disagreementFactor });
         };
     }
 
@@ -13,14 +13,14 @@ function Assert(data, disagreementFactor) {
             personsMatched.add(pair.person2);
         }
 
-        console.assert(personsMatched.size >= data.length, personsMatched, data.length);
+        console.assert(personsMatched.size >= data.length, { matched: personsMatched, total: data.length });
     }
 
     this.noPersonMatchedTwice = function (pairs) {
         var personsMatched = new Set();
         for (let pair of pairs) {
-            console.assert(!personsMatched.has(pair.person1), personsMatched, pair.person1);
-            console.assert(!personsMatched.has(pair.person2), personsMatched, pair.person2);
+            console.assert(!personsMatched.has(pair.person1), { matched: personsMatched, person: pair.person1 });
+            console.assert(!personsMatched.has(pair.person2), { matched: personsMatched, person: pair.person2 });
 
             personsMatched.add(pair.person1);
             personsMatched.add(pair.person2);
